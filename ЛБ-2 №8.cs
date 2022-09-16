f2d741ac7bd0db673466784e86264ced3e08a0c0
DMustacheLaptop <dvusakov+usb@gmail.com>
2022-09-17 01:28:14 +0300

    2 tasks lab 2

---
+++ ЛБ-2 №8.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ЛБ_1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			float bank = 10000;
+			float percent = 0.08f;
+			int months = 0;
+			while (bank < 20000)
+			{
+				months++;
+				bank += bank * percent;
+			}
+			Console.WriteLine(months);
+			Console.ReadLine();
+		}
+	}
+}
---
+++ ЛБ-2 №9.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ЛБ_1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			double l = 0.1;
+			double atom = Math.Pow(10, -10);
+			Console.WriteLine(atom);
+			int cuts = 0;
+			while (l > atom)
+			{
+				l /= 2;
+				cuts++;
+			}
+			Console.WriteLine(cuts);
+			Console.ReadLine();
+		}
+	}
+}
