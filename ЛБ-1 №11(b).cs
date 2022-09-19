70d233f23feca7194812267ad1b7229b7f7b489d
X-xom <71981066+X-xom@users.noreply.github.com>
2022-09-19 19:19:56 +0300

    Create ЛБ-1 №11(b).cs

---
+++ ЛБ-1 №11(b).cs
@@ -0,0 +1,20 @@
+// Напечатать заданную последовательность чисел:
+// 5 5 5 5 5 5
+
+using System;
+
+namespace ЛБ_1__11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 6; i++)
+            {
+                Console.Write("{0} ", 5);
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
