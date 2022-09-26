a431a752d4eb42c82a0e57c8a9c228e3d09615cf
X-xom <71981066+X-xom@users.noreply.github.com>
2022-09-27 02:22:48 +0300

    Update ЛБ-1 №10 lvl2.cs

--- ЛБ-1 №10 lvl2.cs
+++ ЛБ-1 №10 lvl2.cs
@@ -13,14 +13,13 @@ namespace ЛБ_1
 			float denominator_1 = 1;
 			float denominator_2 = 1;
 
-			float addition_del, addition_den;
 			int counter = 2;
 
 			while (Math.Abs(delimiter_1 / denominator_1 - delimiter_2 / denominator_2) > 0.001)
 			{
-			    	counter+=1;
-				addition_del = delimiter_1 + delimiter_2;
-				addition_den = denominator_1 + denominator_2;
+			    counter+=1;
+				float addition_del = delimiter_1 + delimiter_2;
+				float addition_den = denominator_1 + denominator_2;
 				
 				delimiter_2 = delimiter_1;
 				delimiter_1 = addition_del;
